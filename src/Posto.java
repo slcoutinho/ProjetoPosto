f1625801287e828b7ea1f8612b589901d19f7bb8
slcoutinho <slcoutinho@outlook.com>
2020-06-19 18:03:58 -0300

    Sexto Commit

--- src/Posto.java
+++ src/Posto.java
@@ -1,4 +1,34 @@
-
+//BombaDeCombustivel
 public class Posto {
-
+	private String nome;
+	private double vlLitro;
+	private double vlTotal;
+	private double qtdLitros;
+	public Posto (String nome, double vlLitro) {
+		
+	//super();
+	this.nome = nome;
+	this.vlLitro = vlLitro;
+	this.vlTotal = 0.0;
+	this.qtdLitros = 0.0;
+	}
+	
+	public void abastecerPorLitro(double qtdLitros) {
+		this.qtdLitros = qtdLitros; 
+		this.vlTotal = qtdLitros*vlLitro;
+	}
+	
+	public void abastecerPorValor(double total) {
+		this.vlTotal = total;
+		this.qtdLitros = total/vlLitro;
+	}
+	
+	public void exibirRecibo() {
+		System.out.println("******* Posto IsidroCorp Gas & Energy ****");
+		System.out.println(" Combustivel    "+nome);
+		System.out.println(" Valor Litro R$ "+String.format("%.2f",vlLitro));
+		System.out.println(" Quantidade     "+String.format("%.5f",qtdLitros));
+		System.out.println(" Valor Total R$ "+String.format("%.2f",vlTotal));
+		System.out.println("******************************************");
+	}
 }
--- src/PostoTeste.java
+++ src/PostoTeste.java
@@ -1,4 +1,13 @@
 
 public class PostoTeste {
-
+	public static void main(String args[]) {
+		Posto p1 = new Posto ("Gasolina", 4.39);
+		
+		p1.abastecerPorLitro(45);
+		p1.exibirRecibo();
+		
+		System.out.println("\n\n");
+		p1.abastecerPorValor(150.0);
+		p1.exibirRecibo();
+	}
 }
